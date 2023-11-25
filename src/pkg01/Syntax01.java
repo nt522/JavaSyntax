f28254f47b28b800a1f2b49bcc3f2e890486f345
thap965 <emailnawalthapa@gmail.com>
2023-11-24 22:54:01 -0600

    JavaSyntax

---
+++ src/pkg01/Syntax01.java
@@ -0,0 +1,29 @@
+package pkg01;
+
+public class Syntax01 {
+
+	public static void main(String[] args) {
+
+		
+		final int cos = 10;
+		
+		int value2=20;
+		
+		int c = cos + value2;
+		
+		int minus = cos - value2;
+		
+		int multiply = cos * value2;
+		
+		int divide = cos / value2;
+		
+		int remainder = cos % value2;
+		
+		System.out.println(c);
+		System.out.println(minus);
+		System.out.println(multiply);
+		System.out.println(divide);
+		System.out.println(remainder);
+	}
+
+}
